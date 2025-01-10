1fefaabb68c81afffe066dbd4453dbadc4b89033
zaenalrfn <ZAOfficial24@gmail.com>
2025-01-10 23:03:31 +0700

    setup vue js in laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,20 @@
 import "./bootstrap";
+import { createApp } from "vue";
 
 import Alpine from "alpinejs";
 import "flowbite";
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Import komponen Vue Anda
+import LandingPage from "./components/LandingPage.vue";
+
+// Buat aplikasi Vue
+const app = createApp({});
+
+// Register komponen
+app.component("landing-page", LandingPage);
+
+// Mount aplikasi
+app.mount("#app");
---
+++ resources/js/components/LandingPage.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>testing</h1>
+</template>
