30bf31490d75eb95704fe1ba185a6bb479d1fad3
zaenalrfn <ZAOfficial24@gmail.com>
2025-01-24 13:23:45 +0700

    show more effect fade, done

--- resources/js/components/Products.vue
+++ resources/js/components/Products.vue
@@ -2,7 +2,7 @@
 import { useCartStore } from "../stores/cart";
 import { ref } from "vue";
 
-// Mendefinisikan props dengan tipe Array
+// Define props with type Array
 const props = defineProps({
   dataProduct: {
     type: Array,
@@ -10,13 +10,19 @@ const props = defineProps({
   },
 });
 
-// Reactive state untuk menyimpan produk
-const products = ref(props.dataProduct); // Assign props langsung
+// Reactive state for storing products
+const visibleProducts = ref(8); // Initial number of products displayed
+const products = ref(props.dataProduct); // Assign props directly
 
-// bagian cart
+// Cart section
 const cartStore = useCartStore();
 const handleAddToCart = (productId) => {
-  cartStore.addToCart(productId); // Tambahkan produk ke cart
+  cartStore.addToCart(productId); // Add product to cart
+};
+
+// Function to increase the number of displayed products
+const showMoreProducts = () => {
+  visibleProducts.value += 8; // Increase the number of products to display
 };
 </script>
 
@@ -38,107 +44,78 @@ const handleAddToCart = (productId) => {
       <div
         class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4"
       >
-        <div
-          v-for="product in products"
+        <transition-group
+          v-for="(product, index) in products.slice(0, visibleProducts)"
           :key="product.id"
-          class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800"
+          name="fade"
+          tag="div"
         >
-          <div class="h-56 w-full">
-            <a :href="'/detail_products/' + product.id">
-              <img
-                class="mx-auto h-full dark:hidden"
-                :src="'storage/' + product.thumb_image"
-                alt=""
-              />
-              <img
-                class="mx-auto hidden h-full dark:block"
-                :src="'storage/' + product.thumb_image"
-                alt=""
-              />
-            </a>
-          </div>
-          <div class="pt-6">
-            <div class="mb-4 flex items-center justify-between gap-4">
-              <span
-                class="me-2 rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300"
-              >
-                Up to 35% off
-              </span>
+          <div
+            class="h-full rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800"
+          >
+            <div class="h-56 w-full">
+              <a :href="'/detail_products/' + product.id">
+                <img
+                  class="h-full w-full object-cover dark:hidden"
+                  :src="'storage/' + product.thumb_image"
+                  alt=""
+                />
+                <img
+                  class="h-full w-full object-cover hidden dark:block"
+                  :src="'storage/' + product.thumb_image"
+                  alt=""
+                />
+              </a>
             </div>
-
-            <a
-              :href="'/detail_products/' + product.id"
-              class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white"
-              >{{ product.name }}</a
-            >
-
-            <div class="mt-2 flex items-center gap-2">
-              <p class="text-sm font-medium text-gray-500 dark:text-gray-400">
-                (4,775)
-              </p>
-            </div>
-
-            <ul class="mt-2 flex items-center gap-4">
-              <li class="flex items-center gap-2">
-                <svg
-                  class="h-4 w-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
+            <div class="pt-6">
+              <div class="mb-4 flex items-center justify-between gap-4">
+                <span
+                  class="me-2 rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300"
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-width="2"
-                    d="M8 7V6c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1h-1M3 18v-7c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
-                  />
-                </svg>
+                  Up to 35% off
+                </span>
+              </div>
+              <a
+                :href="'/detail_products/' + product.id"
+                class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white"
+                >{{ product.name }}</a
+              >
+              <div class="mt-2 flex items-center gap-2">
                 <p class="text-sm font-medium text-gray-500 dark:text-gray-400">
-                  Best Price
+                  (4,775)
                 </p>
-              </li>
-            </ul>
-
-            <div class="mt-4 flex items-center justify-between gap-4">
-              <p
-                class="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white"
-              >
-                ${{ product.price }}
-              </p>
-
-              <button
-                @click="handleAddToCart(product.id)"
-                type="button"
-                class="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                <svg
-                  class="-ms-2 me-2 h-5 w-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
+              </div>
+              <ul class="mt-2 flex items-center gap-4">
+                <li class="flex items-center gap-2">
+                  <p
+                    class="text-sm font-medium text-gray-500 dark:text-gray-400"
+                  >
+                    Best Price
+                  </p>
+                </li>
+              </ul>
+              <div class="mt-4 flex items-center justify-between gap-4">
+                <p
+                  class="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white"
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
-                  />
-                </svg>
-                Add to cart
-              </button>
+                  ${{ product.price }}
+                </p>
+                <button
+                  @click="handleAddToCart(product.id)"
+                  type="button"
+                  class="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Add to cart
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        </transition-group>
       </div>
-      <div class="w-full text-center">
+      <div v-if="visibleProducts < products.length" class="w-full text-center">
         <button
-          type="button"
-          class="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+          @click="showMoreProducts"
+          class="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700"
         >
           Show more
         </button>
@@ -146,3 +123,23 @@ const handleAddToCart = (productId) => {
     </div>
   </section>
 </template>
+
+<style scoped>
+/* Enhanced Fade In Animation */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease, transform 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
